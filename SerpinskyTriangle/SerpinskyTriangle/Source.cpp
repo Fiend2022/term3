3ad2de61a32672cf72c682ee4320f47753f91351
Fiend2022 <chili_i@mail.ru>
2022-10-14 00:38:54 +0500

    removed magics values

--- SerpinskyTriangle/SerpinskyTriangle/Source.cpp
+++ SerpinskyTriangle/SerpinskyTriangle/Source.cpp
@@ -13,6 +13,10 @@ const short unsigned int radiusOfTriangle = 250;
 const float thickness = 5.f;
 const short unsigned int radiusOfPoint = 2;
 const unsigned int numOfPoints = 250000;
+const sf::Color screenColor = sf::Color::White;
+const sf::Color triangleColor = sf::Color::Red;
+const sf::Color pointColor = sf::Color::Black;
+
 std::random_device rd;
 std::mt19937 gen(rd());
 
@@ -26,7 +30,7 @@ void draw_all_points(sf::RenderWindow& window, std::vector<sf::CircleShape>& poi
 	{
 		std::uniform_int_distribution<int> point(0, 2);
 		sf::CircleShape newPoint(radiusOfPoint);
-		newPoint.setFillColor(sf::Color::White);
+		newPoint.setFillColor(pointColor);
 		size_t index = point(gen);
 		currentPos.x = (pointsOfTriangle[index].x + currentPos.x) / 2;
 		currentPos.y = (pointsOfTriangle[index].y + currentPos.y) / 2;
@@ -40,23 +44,17 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(widthScreen, heidthScreen), "The Serpinsky triangle");
 	window.setFramerateLimit(fps);
 	window.setActive(false);
-	/*sf::CircleShape triangle(radiusOfTriangle, 3);
-	triangle.setPosition(widthScreen / 2 - radiusOfTriangle, heidthScreen / 2 - radiusOfTriangle);
-	triangle.setOutlineThickness(thickness);
-	triangle.setOutlineColor(sf::Color::White);
-	triangle.setFillColor(sf::Color::Black);*/
 	std::vector<sf::Vector2f> pointsOfTriangle;
-	pointsOfTriangle.push_back({widthScreen/2,50});
-	pointsOfTriangle.push_back({ 50, heidthScreen-50});
-	pointsOfTriangle.push_back({widthScreen-50, heidthScreen - 50});
+	pointsOfTriangle.push_back({ widthScreen / 2,heidthScreen * 0.1 });
+	pointsOfTriangle.push_back({ widthScreen * 0.1, heidthScreen * 0.9 });
+	pointsOfTriangle.push_back({ widthScreen * 0.9, heidthScreen * 0.9 });
 	sf::ConvexShape triangle(3);
 	triangle.setPoint(0, pointsOfTriangle[1]);
 	triangle.setPoint(1, pointsOfTriangle[0]);
 	triangle.setPoint(2, pointsOfTriangle[2]);
-	/*get_points(triangle, pointsOfTriangle);*/
-	triangle.setFillColor(sf::Color::Black);
+	triangle.setFillColor(triangleColor);
 	triangle.setOutlineThickness(thickness);
-	triangle.setOutlineColor(sf::Color::White);
+	triangle.setOutlineColor(triangleColor);
 	std::vector<sf::CircleShape> pointsInTriangle;
 	sf::Vector2f currentPos = gen_first_point_in_triangle(pointsOfTriangle, pointsInTriangle);
 	while (window.isOpen())
@@ -67,7 +65,7 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		window.clear(sf::Color::Black);
+		window.clear(screenColor);
 		window.draw(triangle);
 		window.draw(pointsInTriangle[0]);
 		draw_all_points(window, pointsInTriangle, pointsOfTriangle, currentPos);
@@ -83,27 +81,27 @@ void get_points(sf::CircleShape& figure, std::vector<sf::Vector2f>& points)
 
 sf::Vector2f& gen_first_point_in_triangle(std::vector<sf::Vector2f>& pOt, std::vector<sf::CircleShape>& pointsInTriangle)
 {
-	
+
 	float maxY = std::max_element(pOt.begin(), pOt.end(), [](sf::Vector2f& left, sf::Vector2f& right)->bool {return left.y < right.y;})->y;
 	float minY = std::min_element(pOt.begin(), pOt.end(), [](sf::Vector2f& left, sf::Vector2f& right)->bool {return left.y < right.y;})->y;
 	float maxX = std::max_element(pOt.begin(), pOt.end(), [](sf::Vector2f& left, sf::Vector2f& right)->bool {return left.x < right.x;})->x;
 	float minX = std::min_element(pOt.begin(), pOt.end(), [](sf::Vector2f& left, sf::Vector2f& right)->bool {return left.x < right.x;})->x;
 	float h1 = abs(maxY - minY);
 	float l1 = abs(maxX - minX);
-	std::uniform_int_distribution<int> disY(minY+thickness, maxY-thickness);
+	std::uniform_int_distribution<int> disY(minY + thickness, maxY - thickness);
 	int randY = disY(gen);
 	float h2 = abs(minY - randY);
 	float l2 = (h2 / h1) * l1;
-	maxX = widthScreen/2 + l2 /2;
-	minX = widthScreen/2 - l2 / 2;
-	std::uniform_int_distribution<int> disX(minX+1, maxX-1);
+	maxX = widthScreen / 2 + l2 / 2;
+	minX = widthScreen / 2 - l2 / 2;
+	std::uniform_int_distribution<int> disX(minX + 1, maxX - 1);
 	while (1)
 	{
 		int randX = disX(gen);
-		if (randX < maxX-thickness || randX > minX+thickness)
+		if (randX < maxX - thickness || randX > minX + thickness)
 		{
 			sf::CircleShape firstPoint(radiusOfPoint);
-			firstPoint.setFillColor(sf::Color::White);
+			firstPoint.setFillColor(pointColor);
 			sf::Vector2f currentPos(randX, randY);
 			firstPoint.setPosition(currentPos);
 			pointsInTriangle.push_back(firstPoint);
